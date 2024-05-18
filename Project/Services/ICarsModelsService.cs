9775d8cd0e3cd761a9962b026e83b9c6e05b4102
Mateusz <zsuetam36@gmail.com>
2024-05-18 22:34:20 +0200

    refactoring tree cd

--- Project/Services/ICarsModelsService.cs
+++
@@ -1,8 +0,0 @@
-using System.Threading.Tasks;
-
-public interface ICarsModelsService
-{
-    Task CreateCarModelAsync(CarModel carModel);
-    Task<bool> UpdateCarModelAsync(int id, CarModel carModel);
-    Task<bool> DeleteCarModelAsync(int id);
-}
